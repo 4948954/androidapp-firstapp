799dd62cd1b7fc571d6be253e9486fde7c4c69ce
4948954 <aegis.ch@gmail.com>
2013-02-13 23:31:01 -0400

    Feb 13

--- gen/com/shuji/firstapp/R.java
+++ gen/com/shuji/firstapp/R.java
@@ -11,66 +11,71 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int welcome_background=0x7f020001;
+        public static final int firstapp_background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int welcome_background=0x7f020002;
     }
     public static final class id {
-        public static final int analogClock1=0x7f08000a;
-        public static final int bAdd=0x7f080001;
-        public static final int bResults=0x7f08001f;
-        public static final int bReturn=0x7f08001c;
-        public static final int bSA=0x7f08000c;
-        public static final int bSAFR=0x7f08000d;
-        public static final int bSentEmail=0x7f080009;
-        public static final int bSetWall=0x7f080016;
-        public static final int bSub=0x7f080002;
-        public static final int b_magic8ball_start=0x7f08000f;
-        public static final int etAction=0x7f080007;
-        public static final int etEmails=0x7f080003;
-        public static final int etIntro=0x7f080004;
-        public static final int etName=0x7f080005;
-        public static final int etOutro=0x7f080008;
-        public static final int etSend=0x7f08000b;
-        public static final int etThings=0x7f080006;
-        public static final int et_Commands=0x7f080021;
-        public static final int ibTakePic=0x7f080015;
-        public static final int ivReturnedPic=0x7f080014;
-        public static final int menu_settings=0x7f080022;
-        public static final int rBoth=0x7f08001b;
-        public static final int rCrazy=0x7f080019;
-        public static final int rHandsome=0x7f08001a;
-        public static final int radioGroup1=0x7f080010;
-        public static final int rb_magically=0x7f080012;
-        public static final int rb_randomly=0x7f080011;
-        public static final int rgAnswers=0x7f080018;
-        public static final int tgPassword=0x7f08001e;
-        public static final int tvDisplay=0x7f080000;
-        public static final int tvGot=0x7f08000e;
-        public static final int tvQuestion=0x7f080017;
-        public static final int tvResults=0x7f080020;
-        public static final int tvText=0x7f08001d;
-        public static final int tv_Result=0x7f080013;
+        public static final int aboutUs=0x7f090023;
+        public static final int analogClock1=0x7f09000b;
+        public static final int bAdd=0x7f090002;
+        public static final int bResults=0x7f090020;
+        public static final int bReturn=0x7f09001d;
+        public static final int bSA=0x7f09000d;
+        public static final int bSAFR=0x7f09000e;
+        public static final int bSentEmail=0x7f09000a;
+        public static final int bSetWall=0x7f090017;
+        public static final int bSub=0x7f090003;
+        public static final int b_magic8ball_start=0x7f090010;
+        public static final int etAction=0x7f090008;
+        public static final int etEmails=0x7f090004;
+        public static final int etIntro=0x7f090005;
+        public static final int etName=0x7f090006;
+        public static final int etOutro=0x7f090009;
+        public static final int etSend=0x7f09000c;
+        public static final int etThings=0x7f090007;
+        public static final int et_Commands=0x7f090022;
+        public static final int exit=0x7f090025;
+        public static final int ibTakePic=0x7f090016;
+        public static final int ivReturnedPic=0x7f090015;
+        public static final int preferences=0x7f090024;
+        public static final int rBoth=0x7f09001c;
+        public static final int rCrazy=0x7f09001a;
+        public static final int rHandsome=0x7f09001b;
+        public static final int radioGroup1=0x7f090011;
+        public static final int rb_magically=0x7f090013;
+        public static final int rb_randomly=0x7f090012;
+        public static final int rgAnswers=0x7f090019;
+        public static final int tgPassword=0x7f09001f;
+        public static final int tvAbout=0x7f090000;
+        public static final int tvDisplay=0x7f090001;
+        public static final int tvGot=0x7f09000f;
+        public static final int tvQuestion=0x7f090018;
+        public static final int tvResults=0x7f090021;
+        public static final int tvText=0x7f09001e;
+        public static final int tv_Result=0x7f090014;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int email=0x7f030001;
-        public static final int get=0x7f030002;
-        public static final int magic8ball=0x7f030003;
-        public static final int photo=0x7f030004;
-        public static final int send=0x7f030005;
-        public static final int text=0x7f030006;
-        public static final int welcome=0x7f030007;
+        public static final int about=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int email=0x7f030002;
+        public static final int get=0x7f030003;
+        public static final int magic8ball=0x7f030004;
+        public static final int photo=0x7f030005;
+        public static final int send=0x7f030006;
+        public static final int text=0x7f030007;
+        public static final int welcome=0x7f030008;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int cool_menu=0x7f080000;
     }
     public static final class raw {
-        public static final int welcome_bgsong=0x7f040000;
+        public static final int welcome_bgsong=0x7f050000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
     }
     public static final class style {
         /** 
@@ -94,10 +99,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int prefs=0x7f040000;
     }
 }
--- src/com/shuji/firstapp/Menu.java
+++ src/com/shuji/firstapp/Menu.java
@@ -3,36 +3,65 @@ package com.shuji.firstapp;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class Menu extends ListActivity{
+public class Menu extends ListActivity {
 
-	String classes[] = {"MainActivity","TextPlay","Email","Camera","Data","example5","Magic8Ball"};
-	
+	String classes[] = { "MainActivity", "TextPlay", "Email", "Camera", "Data",
+			"example5", "Magic8Ball" };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classes));
+		setListAdapter(new ArrayAdapter<String>(Menu.this,
+				android.R.layout.simple_list_item_1, classes));
 	}
-	
+
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		
+
 		String subApp = classes[position];
-		
-		try{
-		Class ourClass = Class.forName("com.shuji.firstapp." + subApp);
-		Intent ourIntent = new Intent(Menu.this, ourClass);
-		startActivity(ourIntent);
-		} catch(ClassNotFoundException e){
+
+		try {
+			Class ourClass = Class.forName("com.shuji.firstapp." + subApp);
+			Intent ourIntent = new Intent(Menu.this, ourClass);
+			startActivity(ourIntent);
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	
-	
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu);
+
+		MenuInflater blowup = getMenuInflater();
+		blowup.inflate(R.menu.cool_menu, menu);
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.aboutUs:
+			Intent i =new Intent("com.shuji.firstapp.ABOUTUS");
+			startActivity(i);
+			break;
+		case R.id.preferences:
+			
+			break;
+		}
+
+		return false;
+	}
+
 }
